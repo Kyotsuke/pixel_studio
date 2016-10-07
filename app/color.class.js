e7c5d6ccdc04b97372516323ac7de6fe97172f3a
Kyotsuke <mattieu.vendeville@gmail.com>
2016-10-07 11:16:00 +0200

    Mise en place des dossiers et d'index.html ; projet vide ready

---
+++ app/color.class.js
@@ -0,0 +1,12 @@
+class Color{
+
+	/**
+	 * Représente une couleur dans la palette
+	 * @param  {string} name Nom de l'outil
+	 * @param  {array} color Tableau des composantes R, G, B
+	 */
+	constructor(name, color){
+		this.name = name;
+		this.color = color;
+	}
+}
---
+++ app/tool.class.js
@@ -0,0 +1,12 @@
+class Tool{
+
+	/**
+	 * Présente un outil dans la palette
+	 * @param  {string} name Nom de l'outil
+	 * @param  {string} icon Chemin de l'icône
+	 */
+	constructor(name, icon){
+		this.name = name;
+		this.icon = icon;
+	}
+}
